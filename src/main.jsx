fa228dc46e37b8ef7ee82200eb6c459483da9735
Zuhair <zuhairahmed055@gmail.com>
2023-04-29 06:37:33 +0530

    env keys test

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-// import MapForm from "./components/MapForm";
+// import App from "./App";
+import MapForm from "./components/MapForm";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const container = document.getElementById("root");
@@ -9,7 +9,7 @@ const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/*" element={<App />} />
+      <Route path="/*" element={<MapForm />} />
     </Routes>
   </BrowserRouter>
 );

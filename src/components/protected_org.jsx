c666ec7e2399830f002a59b527fc59095484731e
Zuhair <zuhairahmed055@gmail.com>
2023-06-28 11:42:15 +0530

    all images staticly rendered

--- src/components/protected_org.jsx
+++ src/components/protected_org.jsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "../api/axios";
-import { Button, Typography, Grid, Paper, Box } from "@mui/material";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Typography,
+  Button,
+  Box,
+  Grid,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import image from "../images/Img1.png";
 
@@ -18,11 +29,6 @@ const Container = styled(Paper)(({ theme }) => ({
   backgroundAttachment: "fixed",
 }));
 
-const Title = styled(Typography)(({ theme }) => ({
-  fontSize: "2.5rem",
-  marginBottom: theme.spacing(4),
-  color: "#444444",
-}));
 const CustomButton = styled(Button)`
   padding: 1rem 2rem;
   background-color: #d62828;
@@ -39,26 +45,13 @@ const CustomButton = styled(Button)`
   }
 `;
 
-const Subtitle = styled(Typography)(({ theme }) => ({
-  marginTop: theme.spacing(4),
-  marginBottom: theme.spacing(2),
-  color: "#444444",
-}));
-
-const DataList = styled(Grid)(({ theme }) => ({
-  marginTop: theme.spacing(4),
-}));
-
-const DataListItem = styled(Typography)(({ theme }) => ({
-  fontSize: "1.5rem",
-  marginBottom: theme.spacing(2),
-  color: "#444444",
-}));
-
 function Protected_Org() {
   const { id } = useParams();
   const [orgData, setOrgData] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [selectedVehicles, setSelectedVehicles] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
 
   useEffect(() => {
     const fetch = async () => {
@@ -67,6 +60,9 @@ function Protected_Org() {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         });
+        setSelectedVehicles(response.data.selected_vehicle_ids);
+        setLat(response.data.lat);
+        setLng(response.data.lng);
         setOrgData(response.data);
         console.log(response.data);
         if (response.data._id) setIsAuthenticated(true);
@@ -82,45 +78,126 @@ function Protected_Org() {
   return (
     <>
       {!isAuthenticated ? (
-        <h1>Loading</h1>
+        <Container>
+          <h1>Loading</h1>
+        </Container>
       ) : (
         <Container elevation={3}>
-          <Box sx={{ maxWidth: "800px", width: "100%" }}>
-            <Title
-              sx={{ fontFamily: "Arial", fontSize: "2.5rem", color: "#444444" }}
-              variant="h1"
+          <Box
+            sx={{
+              width: "100%",
+              minHeight: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Grid item xs={12} sx={{ mt: 4 }}>
+              <Typography variant="h2">Welcome {orgData?.name}!</Typography>
+            </Grid>
+            <div
+              style={{ flexGrow: 1, width: "100%", justifyContent: "center" }}
             >
-              <h1> WELCOME {orgData?.name}</h1>
-            </Title>
-
-            <Title
-              sx={{ fontFamily: "Arial", fontSize: "2.5rem", color: "#444444" }}
-              variant="h1"
-            >
-              PROTECTED ORGANISATION PAGE
-            </Title>
-
-            <DataList container>
-              <Grid item xs={12}>
-                <Subtitle variant="h2">Organization Info:</Subtitle>
-              </Grid>
-              <Grid item xs={12}>
-                <DataListItem variant="h3">ID: {orgData?._id}</DataListItem>
-              </Grid>
-              <Grid item xs={12}>
-                <DataListItem variant="h3">Name: {orgData?.name}</DataListItem>
-              </Grid>
-              <Grid item xs={12}>
-                <DataListItem variant="h3">
-                  Email: {orgData?.email}
-                </DataListItem>
-              </Grid>
-              <Grid item xs={12}>
+              <Paper elevation={3} style={{ width: "100%", padding: "16px" }}>
+                <Typography variant="h4" style={{ marginBottom: "16px" }}>
+                  Your Registered Commute Programs:
+                </Typography>
+                <Table>
+                  <TableHead>
+                    <TableRow style={{ backgroundColor: "#f8f8f8" }}>
+                      <TableCell>
+                        <Typography variant="h6">Name</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">Type</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">From</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">To</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">Number</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">Arrival Time</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">Departure Time</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">Available Seats</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="h6">Map View</Typography>
+                      </TableCell>
+                      <TableCell></TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {selectedVehicles.map((vehicle, index) => (
+                      <TableRow
+                        key={index}
+                        style={{
+                          backgroundColor: index % 2 ? "#f8f8f8" : "white",
+                        }}
+                      >
+                        <TableCell>
+                          <Typography>{vehicle.name}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.type}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.from}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.to}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.number}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.arrivalTime}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.departureTime}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Typography>{vehicle.available_seats}</Typography>
+                        </TableCell>
+                        <TableCell>
+                          <Box
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                          >
+                            <Link
+                              to={`/vehicle/${vehicle._id}?lat=${lat}&lng=${lng}`}
+                            >
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                style={{ fontWeight: "bold" }}
+                              >
+                                Map View
+                              </Button>
+                            </Link>
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+              <Grid item xs={12} sx={{ mt: 4, justifyContent: "center" }}>
                 <Link to={"/organization/" + orgData?._id + "/vehicleForm"}>
                   <CustomButton>Add commute details</CustomButton>
                 </Link>
               </Grid>
-            </DataList>
+            </div>
           </Box>
         </Container>
       )}
